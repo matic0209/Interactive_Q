ce20d6a74a1f7fcc617bcb12a1e11ea7db7f316f
zhangjinbopku <cszhangjinbo@gmail.com>
2014-11-10 21:35:43 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iterator>
+#include <iostream>
+#include <omp.h>
+#include <time.h>
+#include <string.h>
+#include <boost/lexical_cast.hpp>
+#include <time.h>
+#include <iostream>
+#include <algorithm>
+
+
+int main(int argc, char *argv[])
+{
+
+    using namespace std;
+
+    std::cout <<"Hello world:" << std::endl;
+    
+    return 0;
+}
